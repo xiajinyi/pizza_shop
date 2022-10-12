b273b53a52ce866067f4b2de39d2e1be1656f67a
Geraldine Klarenberg <gklarenberg@users.noreply.github.com>
2022-10-12 14:29:25 -0400

    Added calculations and plots to the R script. Added .DS_Store to .gitignore. Created a readme file.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Pizza shop
+
+This repository contains files and data related to a fictional pizzeria. It contains a text file with ideas for different types of pizza, and sales data. It also includes an R script to analyze sales data. 
--- sales/sales_script.R
+++ sales/sales_script.R
@@ -1,3 +1,34 @@
 # Pizza sales
 
+##### 1. Load packages #######
+library(tidyverse)
+library(lubridate)
+
+##### 2. Load data #######
 sales_data <- read.csv("sales/202003_sales.csv")
+
+##### 3. Create summaries #####
+
+# Monthly sales per pizza type
+sales_summary <- sales_data %>%
+  group_by(pizza, month) %>% 
+  summarize(total_sales = sum(number))
+
+ggplot(data = sales_summary, aes(x = pizza, y = total_sales))+
+  geom_bar(stat = "identity")
+
+# Daily sales
+# Create "proper" dates
+sales_data$date <- ymd(paste(sales_data$year, "/", sales_data$month, "/", sales_data$day))  
+
+# Summarize data
+sales_summary_daily <- sales_data %>%
+  group_by(pizza, date) %>% 
+  summarize(total_sales = sum(number))
+
+# Plot
+ggplot(data = sales_summary_daily, aes(x = date, y = total_sales, color = pizza))+
+  geom_line()
+
+ggplot(data = sales_summary_daily, aes(x = date, y = total_sales, fill = pizza))+
+  geom_bar(stat = "identity")
