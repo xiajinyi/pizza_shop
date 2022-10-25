767e4f3b23f2afb11b58afa72bf6d53604a07075
czeidan <czeidan@ufl.edu>
2022-10-25 16:45:52 -0400

    First committ.

--- sales/sales_script.R
+++ sales/sales_script.R
@@ -6,25 +6,34 @@ library(lubridate)
 
 ##### 2. Load data #######
 sales_data <- read.csv("sales/202003_sales.csv")
+dim(sales_data) # 62 rows and 5 columns
+str(sales_data) # integer, character data
+summary(sales_data) # describes min, median, etc. of each column
+View(sales_data) # view data and skim for any NAs etc.
 
 ##### 3. Create summaries #####
 
 # Monthly sales per pizza type
-sales_summary <- sales_data %>%
-  group_by(pizza, month) %>% 
-  summarize(total_sales = sum(number))
+sales_summary <- sales_data %>% # calls data from sales_data to create new
+  group_by(pizza, month) %>%  # groups pizza by month (ties each data point of pizza to the 
+                              # month it was purchased in)
+  summarize(total_sales = sum(number)) # creates total_sales as y-axis and input is the sum
+                                      # of the number of pizza sold
 
 ggplot(data = sales_summary, aes(x = pizza, y = total_sales))+
   geom_bar(stat = "identity")
+# makes x-axis pizza (which is now grouped by month) and y-axis total sales 
+# (total sum of the number of types of pizza for that month)
 
 # Daily sales
 # Create "proper" dates
 sales_data$date <- ymd(paste(sales_data$year, "/", sales_data$month, "/", sales_data$day))  
 
 # Summarize data
-sales_summary_daily <- sales_data %>%
-  group_by(pizza, date) %>% 
-  summarize(total_sales = sum(number))
+sales_summary_daily <- sales_data %>% # creating summary of daily sales from sales_data
+  group_by(pizza, date) %>% # grouping pizza by date 
+  summarize(total_sales = sum(number)) # creates total_sales as y-axis and input is the sum
+                                      # of the number of pizza sold
 
 # Plot
 ggplot(data = sales_summary_daily, aes(x = date, y = total_sales, color = pizza))+
